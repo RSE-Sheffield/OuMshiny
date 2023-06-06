8af47317d9cc6baf6397c17fb38f39811469aad4
Dan Brady <daniel.brady@sheffield.ac.uk>
2023-06-06 15:52:38 +0100

    using wordcloud 2 for slightly more interactive plots

--- R/mod_wordcloud.R
+++ R/mod_wordcloud.R
@@ -7,33 +7,38 @@
 #' @noRd
 #'
 #' @importFrom shiny NS tagList
+#' @import wordcloud2
 mod_wordcloud_ui <- function(id){
   ns <- NS(id)
   tabPanel(
     title <- id,
     selectInput(ns("dataset"), "Select Dataset", c("vaccine", "brexit", "veganism")),
-    plotOutput(ns("plot"))
+    fluidRow(
+      column(5,
+            selectInput(ns("arg_pos1"), "Select Argument position", c("For", "Against")),
+            selectInput(ns("arguer_pos1"), "Select Arguer position", c("Pro", "Anti") ),
+            wordcloud2Output(ns("plot1"))
+      )
+    )
   )
 }
+
 
 #' mod_wordcloud Server Functions
 #'
 #' @noRd
 #'
-#' @import ggplot ggwordcloud
+#' @import wordcloud2
 mod_wordcloud_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
-    word_freqs <- reactive({
-      data <- get_data(input$dataset)
-      word_freqs <- generate_word_freqs(data)
-    })
 
-    output$plot <- renderPlot({
-      ggplot(word_freqs()) +
-        geom_text_wordcloud_area(aes(label = word, size = n)) +
-        scale_size_area(max_size = 15)
-      })
+    output$plot1 <- renderWordcloud2({
+      data <- get_data(input$dataset)
+      data <- filter(data, argument_position == input$arg_pos1)
+      word_freqs <- generate_word_freqs(data)
+      wordcloud2(word_freqs, rotateRatio = 0)
+    })
 
   })
 }
