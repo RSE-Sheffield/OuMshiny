350d0d07c2e259580cf0b14dad5abdfe0ebee9d8
Dan Brady <daniel.brady@sheffield.ac.uk>
2023-06-06 18:10:22 +0100

    Added rest of UI elements and corresponding server functions

--- R/mod_wordcloud.R
+++ R/mod_wordcloud.R
@@ -15,8 +15,9 @@ mod_wordcloud_ui <- function(id){
     selectInput(ns("dataset"), "Select Dataset", c("vaccine", "brexit", "veganism")),
     fluidRow(
       column(5,
-            selectInput(ns("arg_pos"), "Select Arguer position", c(" ", "Pro", "Anti")),
-            selectizeInput(ns("condition"), "Select Condition", c(" ", "ITT", "Baseline") ),
+            selectInput(ns("arg_pos"), "Filter by Arguer position", c("All", "Pro", "Anti")),
+            selectInput(ns("condition"), "Filter by Condition", c("All", "ITT", "Baseline") ),
+            sliderInput(ns("rating"), "Filter by Rating", min = 1, max = 7, value = c(1,7)),
             wordcloud2Output(ns("plot"))
       )
     )
@@ -35,8 +36,9 @@ mod_wordcloud_server <- function(id){
 
     filtered_data <- reactive({
       data <- get_data(input$dataset)
-      if (input$arg_pos != " ") data <- filter(data, arguer_position == input$arg_pos)
-      if (input$condition != " ") data <- filter(data, condition == input$condition)
+      data <- filter(data, mean_rating >= input$rating[1] & mean_rating <= input$rating[2])
+      if (input$arg_pos != "All") data <- filter(data, arguer_position == input$arg_pos)
+      if (input$condition != "All") data <- filter(data, condition == input$condition)
       return(data)
     })
 
