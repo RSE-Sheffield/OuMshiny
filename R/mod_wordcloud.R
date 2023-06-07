dd3bd626499f3e226f79a3c9b1a571bca760ad5e
Dan Brady <daniel.brady@sheffield.ac.uk>
2023-06-07 15:26:08 +0100

    Updeated wordcloud mod with more informative selection labels

--- R/mod_wordcloud.R
+++ R/mod_wordcloud.R
@@ -12,12 +12,15 @@ mod_wordcloud_ui <- function(id){
   ns <- NS(id)
   tabPanel(
     title <- id,
-    selectInput(ns("dataset"), "Select Dataset", c("vaccine", "brexit", "veganism")),
+    selectInput(ns("dataset"), "Select Dataset", c("Covid-19 Vaccine" = "vaccine",
+                                                   "Brexit" = "brexit",
+                                                   "Veganism" = "veganism")),
+    hr(),
     fluidRow(
       column(5,
-            selectInput(ns("arg_pos"), "Filter by Arguer position", c("All", "Pro", "Anti")),
-            selectInput(ns("condition"), "Filter by Condition", c("All", "ITT", "Baseline") ),
-            sliderInput(ns("rating"), "Filter by Rating", min = 1, max = 7, value = c(1,7)),
+            selectInput(ns("arg_pos"), "Filter by Arguer position", choices = NULL),
+            selectInput(ns("condition"), "Filter by Condition", c("ITT", "Baseline") ),
+            sliderInput(ns("rating"), "Filter by Rating", min = 1, max = 7, value = c(1,7), step = 0.5),
             wordcloud2Output(ns("plot"))
       )
     )
@@ -34,11 +37,15 @@ mod_wordcloud_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
 
+    observeEvent(input$dataset, {
+      updateSelectInput(session, "arg_pos", choices = generate_input_list(input$dataset))
+    })
+
     filtered_data <- reactive({
       data <- get_data(input$dataset)
-      data <- filter(data, mean_rating >= input$rating[1] & mean_rating <= input$rating[2])
-      if (input$arg_pos != "All") data <- filter(data, arguer_position == input$arg_pos)
-      if (input$condition != "All") data <- filter(data, condition == input$condition)
+      data <- filter(data,
+                     mean_rating >= input$rating[1] & mean_rating <= input$rating[2],
+                     arguer_position == input$arg_pos, condition == input$condition)
       return(data)
     })
 
